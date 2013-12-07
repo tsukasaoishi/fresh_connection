48543319e97de61e782fdc3def5019d845980fbd
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-12-07 21:47:24 +0900

    Ignore model is always ignore.

--- lib/fresh_connection/active_record/relation.rb
+++ lib/fresh_connection/active_record/relation.rb
@@ -5,7 +5,9 @@ module ActiveRecord
     def exec_queries_with_slave_connection
       return @records if loaded?
 
-      if go_slave?
+      if FreshConnection::SlaveConnection.ignore_model?(@klass.name)
+        exec_queries_without_slave_connection
+      elsif go_slave?
         FreshConnection::SlaveConnection.slave_access { exec_queries_without_slave_connection }
       else
         FreshConnection::SlaveConnection.master_access { exec_queries_without_slave_connection }
@@ -14,8 +16,7 @@ module ActiveRecord
     alias_method_chain :exec_queries, :slave_connection
 
     def go_slave?
-      !FreshConnection::SlaveConnection.ignore_model?(@klass.name) &&
-        connection.open_transactions == 0 && (@readonly_value.nil? || @readonly_value)
+      connection.open_transactions == 0 && (@readonly_value.nil? || @readonly_value)
     end
   end
 end
