58a156f6ad26164daf7e2a9251615ea58a80ef96
tsukasaoishi <tsukasa.oishi@gmail.com>
2013-12-12 22:10:32 +0900

    supports count method

--- lib/fresh_connection/active_record/relation.rb
+++ lib/fresh_connection/active_record/relation.rb
@@ -1,5 +1,12 @@
 module ActiveRecord
   class Relation
+    def calculate_with_slave_connection(operation, column_name, options = {})
+      FreshConnection::SlaveConnection.manage_access(@klass, (go_slave? && options[:readonly] != false)) do
+        calculate_without_slave_connection(operation, column_name, options)
+      end
+    end
+    alias_method_chain :calculate, :slave_connection
+
     private
 
     def exec_queries_with_slave_connection
