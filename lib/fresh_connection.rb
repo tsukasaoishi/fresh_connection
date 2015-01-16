7a9b55d084501c00c3857752d2d6e82d66774510
Tsukasa OISHI <oishi_t@kakaku.com>
2015-01-16 14:24:59 +0900

    remove String object support

--- lib/fresh_connection.rb
+++ lib/fresh_connection.rb
@@ -1,5 +1,4 @@
 require 'active_record'
-require 'active_support/core_ext'
 require 'active_support/dependencies/autoload'
 
 module FreshConnection
@@ -14,8 +13,7 @@ module FreshConnection
     attr_writer :connection_manager, :env
 
     def connection_manager
-      manager_klass = @connection_manager || ConnectionManager
-      manager_klass.is_a?(String) ? manager_klass.constantize : manager_klass
+      @connection_manager || ConnectionManager
     end
 
     def env
