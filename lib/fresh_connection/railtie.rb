fded92e0674e098609768835b1e3dba5bc549524
Tsukasa OISHI <oishi_t@kakaku.com>
2015-01-19 09:31:58 +0900

    wait load to activerecord

--- lib/fresh_connection/railtie.rb
+++ lib/fresh_connection/railtie.rb
@@ -3,10 +3,12 @@ require 'fresh_connection/rack/connection_management'
 module FreshConnection
   class Railtie < Rails::Railtie
     initializer "fresh_connection.configure_rails_initialization" do |app|
-      app.config.app_middleware.swap(
-        ActiveRecord::ConnectionAdapters::ConnectionManagement,
-        FreshConnection::Rack::ConnectionManagement
-      )
+      ActiveSupport.on_load(:active_record) do
+        app.config.app_middleware.swap(
+          ActiveRecord::ConnectionAdapters::ConnectionManagement,
+          FreshConnection::Rack::ConnectionManagement
+        )
+      end
     end
   end
 end
