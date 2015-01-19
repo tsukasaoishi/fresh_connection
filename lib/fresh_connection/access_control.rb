c0dd4c2631c2302508e3ac281da6f10b741474ac
Tsukasa OISHI <oishi_t@kakaku.com>
2015-01-19 10:13:03 +0900

    remove unnecessary counter

--- lib/fresh_connection/access_control.rb
+++ lib/fresh_connection/access_control.rb
@@ -1,19 +1,17 @@
 module FreshConnection
   class AccessControl
     class << self
-      def force_master_access
-        db = access_db
-        access_to(:master)
-        yield
-      ensure
-        access_to(db)
+      def force_master_access(&block)
+        switch_to(:master, &block)
       end
 
-      def access(enable_slave_access)
-        access_in(enable_slave_access ? :slave : :master)
-        yield
-      ensure
-        access_out
+      def access(enable_slave_access, &block)
+        if access_db
+          block.call
+        else
+          db = enable_slave_access ? :slave : :master
+          switch_to(db, &block)
+        end
       end
 
       def slave_access?
@@ -22,14 +20,12 @@ module FreshConnection
 
       private
 
-      def access_in(db)
-        increment_access_count
-        access_to(db) unless access_db
-      end
-
-      def access_out
-        decrement_access_count
-        access_to(nil) if access_count == 0
+      def switch_to(new_db)
+        old_db = access_db
+        access_to(new_db)
+        yield
+      ensure
+        access_to(old_db)
       end
 
       def access_db
@@ -38,18 +34,6 @@ module FreshConnection
 
       def access_to(db)
         Thread.current[:fresh_connection_access_target] = db
-      end
-
-      def access_count
-        Thread.current['fresh_connection_access_count'] || 0
-      end
-
-      def increment_access_count
-        Thread.current['fresh_connection_access_count'] = access_count + 1
-      end
-
-      def decrement_access_count
-        Thread.current['fresh_connection_access_count'] = access_count - 1
       end
     end
   end
