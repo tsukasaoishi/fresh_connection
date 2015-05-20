fb18a6ebd5594b2bfb5e3d0006d8db50bd893b79
tsukasaoishi <tsukasa.oishi@gmail.com>
2015-05-20 23:31:46 +0900

    fix wrong timing

--- lib/fresh_connection/railtie.rb
+++ lib/fresh_connection/railtie.rb
@@ -3,12 +3,10 @@ require 'fresh_connection/rack/connection_management'
 module FreshConnection
   class Railtie < Rails::Railtie
     initializer "fresh_connection.configure_rails_initialization" do |app|
-      ActiveSupport.on_load(:active_record) do
-        app.config.app_middleware.insert_after(
-          ActiveRecord::ConnectionAdapters::ConnectionManagement,
-          FreshConnection::Rack::ConnectionManagement
-        )
-      end
+      app.config.app_middleware.insert_before(
+        ActiveRecord::ConnectionAdapters::ConnectionManagement,
+        FreshConnection::Rack::ConnectionManagement
+      )
     end
   end
 end
