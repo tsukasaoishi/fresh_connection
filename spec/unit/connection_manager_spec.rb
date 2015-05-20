0e177faa9a4ccc4d0d0cdebbe52f6b966ceb0fd0
tsukasaoishi <tsukasa.oishi@gmail.com>
2015-05-21 00:11:49 +0900

    put_aside disconnect only current connection

--- spec/unit/connection_manager_spec.rb
+++ spec/unit/connection_manager_spec.rb
@@ -36,6 +36,30 @@ describe FreshConnection::ConnectionManager do
   end
 
   context "#put_aside!" do
+    it "current thread connection disconnect" do
+      threads_num = 5
+      threads = []
+      threads_num.times do |i|
+        threads << Thread.new do
+          @cm.slave_connection
+        end
+      end
+      threads.each(&:join)
+
+      current_connection = @cm.slave_connection
+      @cm.put_aside!
+      expect(current_connection.active?).to be_falsey
+
+      connections = @cm.instance_variable_get("@slave_connections").values
+      expect(connections.size).to eq(threads_num)
+      connections.each do |c|
+        expect(c.active?).to be_truthy
+        expect(c).not_to equal(current_connection)
+      end
+    end
+  end
+
+  context "#clear_all_connections!" do
     it "all connections disconnect" do
       threads_num = 5
       threads = []
@@ -48,7 +72,7 @@ describe FreshConnection::ConnectionManager do
 
       connections = @cm.instance_variable_get("@slave_connections").values
 
-      @cm.put_aside!
+      @cm.clear_all_connections!
       expect(@cm.instance_variable_get("@slave_connections")).to be_empty
       connections.each do |c|
         expect(c.active?).to be_falsey
