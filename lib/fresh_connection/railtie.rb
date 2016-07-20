76026fc802dbf3ec3b812363abad5630e3961be9
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2016-07-20 14:09:30 +0900

    Rails5.0 has not ActiveRecord::ConnectionAdapters::ConnectionManagement

--- lib/fresh_connection/railtie.rb
+++ lib/fresh_connection/railtie.rb
@@ -3,10 +3,17 @@ require 'fresh_connection/rack/connection_management'
 module FreshConnection
   class Railtie < Rails::Railtie
     initializer "fresh_connection.configure_rails_initialization" do |app|
-      app.config.app_middleware.insert_before(
-        ActiveRecord::ConnectionAdapters::ConnectionManagement,
-        FreshConnection::Rack::ConnectionManagement
-      )
+      if defined?(ActiveRecord::ConnectionAdapters::ConnectionManagement)
+        app.config.app_middleware.insert_before(
+          ActiveRecord::ConnectionAdapters::ConnectionManagement,
+          FreshConnection::Rack::ConnectionManagement
+        )
+      else
+        app.config.app_middleware.insert_before(
+          ActionDispatch::Reloader,
+          FreshConnection::Rack::ConnectionManagement
+        )
+      end
     end
   end
 end
