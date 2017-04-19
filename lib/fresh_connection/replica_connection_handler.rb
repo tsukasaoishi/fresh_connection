95d96e06ef8cd99874233e6ea4445aff987f4f04
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2017-04-20 05:42:47 +0900

    connection_manager object is lazy evaluation

--- lib/fresh_connection/replica_connection_handler.rb
+++ lib/fresh_connection/replica_connection_handler.rb
@@ -3,15 +3,16 @@ require 'concurrent'
 module FreshConnection
   class ReplicaConnectionHandler
     def initialize
+      @replica_group_to_pool = Concurrent::Map.new
       @class_to_pool = Concurrent::Map.new
     end
 
     def establish_connection(name, replica_group)
-      if cm = class_to_pool[name]
+      if cm = class_to_pool(name)
         cm.put_aside!
       end
 
-      class_to_pool[name] = FreshConnection.connection_manager.new(replica_group)
+      @replica_group_to_pool[name] = replica_group
     end
 
     def connection(klass)
@@ -41,20 +42,23 @@ module FreshConnection
     private
 
     def all_connection_managers
-      class_to_pool.each_value do |connection_manager|
+      @class_to_pool.each_value do |connection_manager|
         yield(connection_manager)
       end
     end
 
     def detect_connection_manager(klass)
-      c = class_to_pool[klass.name]
+      c = class_to_pool(klass.name)
       return c if c
       return nil if ActiveRecord::Base == klass
       detect_connection_manager(klass.superclass)
     end
 
-    def class_to_pool
-      @class_to_pool
+    def class_to_pool(name)
+      return @class_to_pool[name] if @class_to_pool.key?(name)
+      g = @replica_group_to_pool[name]
+      return nil unless g
+      @class_to_pool[name] = FreshConnection.connection_manager.new(g)
     end
   end
 end
