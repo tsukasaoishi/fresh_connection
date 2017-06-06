f324db70ee1604d3a53ae8b6d00de545b8e99a81
Alan Stebbens <alan.stebbens@procore.com>
2017-06-05 22:53:18 -0700

    Revert "possible fix to query cache problem"
    This reverts commit 8a1e2440efa19a32624995c0860afe654a2a4778.

--- lib/fresh_connection/extend/ar_abstract_adapter.rb
+++ lib/fresh_connection/extend/ar_abstract_adapter.rb
@@ -9,12 +9,6 @@ module FreshConnection
         args[1] = "[#{@replica_group}] #{args[1]}" if defined?(@replica_group)
         super
       end
-
-      # This is normally called only on master connections, so we need
-      # to clear the replica connection caches, too.  But, don't recurse.
-      #def clear_query_cache
-      #  replica_connection.clear_query_cache unless replica_group
-      #end
     end
   end
 end
