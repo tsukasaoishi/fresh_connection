48f38a1a010540df38053de5e75d5af7341b9312
Alan Stebbens <alan.stebbens@procore.com>
2017-06-05 22:51:35 -0700

    support fake replica (using master)

--- test/config/prepare_mysql2_url.rb
+++ test/config/prepare_mysql2_url.rb
@@ -10,10 +10,11 @@ db_user += '@' if db_user
 
 db_params = [ 'encoding=utf8', 'pool=5' ].join('&')
 
-ENV['DB_ADAPTER']            = 'mysql2'
-ENV['DB_USER']               = mysql_user
-ENV['DATABASE_URL']          = "mysql2://#{db_user}localhost/#{dbname}?#{db_params}"
-ENV['DATABASE_REPLICA_URL']  = "mysql2://#{db_user}localhost/#{replica1_dbname}?#{db_params}"
-ENV['DATABASE_REPLICA1_URL'] = "mysql2://#{db_user}localhost/#{replica1_dbname}?#{db_params}"
-ENV['DATABASE_REPLICA2_URL'] = "mysql2://#{db_user}localhost/#{replica2_dbname}?#{db_params}"
+ENV['DB_ADAPTER']                = 'mysql2'
+ENV['DB_USER']                   = mysql_user
+ENV['DATABASE_URL']              = "mysql2://#{db_user}localhost/#{dbname}?#{db_params}"
+ENV['DATABASE_REPLICA_URL']      = "mysql2://#{db_user}localhost/#{replica1_dbname}?#{db_params}"
+ENV['DATABASE_REPLICA1_URL']     = "mysql2://#{db_user}localhost/#{replica1_dbname}?#{db_params}"
+ENV['DATABASE_REPLICA2_URL']     = "mysql2://#{db_user}localhost/#{replica2_dbname}?#{db_params}"
+ENV['DATABASE_FAKE_REPLICA_URL'] = "mysql2://#{db_user}localhost/#{dbname}?#{db_params}"
 
