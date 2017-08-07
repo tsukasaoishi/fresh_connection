fe940740566cccf46c08acd3b5df82418d30569c
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2017-08-07 16:49:37 +0900

    Fixed method name error

--- lib/fresh_connection/deprecation.rb
+++ lib/fresh_connection/deprecation.rb
@@ -4,7 +4,7 @@ module FreshConnection
   class Deprecation
     class << self
       def warn(list = {})
-        list.each do |new_method, old_method|
+        list.each do |old_method, new_method|
           ActiveSupport::Deprecation.warn(
             "'#{old_method}' is deprecated and will removed from version 2.5.0. use '#{new_method}' instead."
           )
